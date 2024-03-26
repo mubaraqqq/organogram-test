6f39980d207eefd927e631bb7995465a1e7be99d
Mubaraq Mustapha <ayomidemusty@gmail.com>
2024-03-26 14:19:03 +0100

    fix: edit metadata

--- src/constant/config.ts
+++ src/constant/config.ts
@@ -1,6 +1,6 @@
 export const siteConfig = {
-  title: "GoPot Admin",
-  description: "Challenge your Friends, Thousands of People Around The World",
+  title: 'Organogram',
+  description: 'Question diary',
   /** Without additional '/' on the end, e.g. https://theodorusclarence.com */
-  url: "https://tsnext-tw.thcl.dev",
+  url: 'https://tsnext-tw.thcl.dev',
 };
